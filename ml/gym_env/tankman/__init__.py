ec79603fbeab1f829063b6ddb8ce6aeeaf282b54
Jerry Lee <oojerryleeoo@gmail.com>
2023-06-09 14:03:10 +0800

    Naming update

--- ml/gym_env/tankman/__init__.py
+++ ml/gym_env/tankman/__init__.py
@@ -2,9 +2,9 @@ from gymnasium.envs.registration import register
 
 register(
     id="TankManResupply-v0",
-    entry_point="gym_env.tankman.resupply_env:ResupplyEnv",
+    entry_point="gym_env.tankman.resupply_env_v0:ResupplyEnv",
 )
 register(
     id="TankManShooter-v0",
-    entry_point="gym_env.tankman.shooter_env:ShooterEnv",
+    entry_point="gym_env.tankman.shooter_env_v0:ShooterEnv",
 )
